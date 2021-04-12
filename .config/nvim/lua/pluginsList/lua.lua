01b1711d524f1dea127fc48b4c0823f99bc48a37
Nguyen Thanh Dat <bacbia3696@gmail.com>
2021-04-12 11:48:47 +0700

    update config

--- .config/nvim/lua/pluginsList/lua.lua
+++ .config/nvim/lua/pluginsList/lua.lua
@@ -4,6 +4,7 @@ return require("packer").startup(
     use {'wbthomason/packer.nvim'}
 
     use {'svermeulen/vimpeccable', opt = true}
+    use 'nvim-lua/plenary.nvim'
     use 'tjdevries/nlua.nvim'
 
     use 'tpope/vim-surround'
@@ -12,31 +13,30 @@ return require("packer").startup(
     use 'neovim/nvim-lspconfig'
     use 'kabouzeid/nvim-lspinstall'
     use 'hrsh7th/nvim-compe'
+    use 'nvim-treesitter/nvim-treesitter'
+    use 'sbdchd/neoformat'
+    use 'terrortylor/nvim-comment'
+    use 'sheerun/vim-polyglot'
 
     use 'srcery-colors/srcery-vim'
     use 'kyazdani42/nvim-web-devicons'
     use 'kyazdani42/nvim-tree.lua'
     use 'glepnir/galaxyline.nvim'
-    use 'glepnir/dashboard-nvim'
+    use 'norcalli/nvim-colorizer.lua'
+    -- use 'glepnir/dashboard-nvim'
+    use 'tweekmonster/startuptime.vim'
+    use 'ryanoasis/vim-devicons'
 
     use 'hrsh7th/vim-vsnip'
     use 'hrsh7th/vim-vsnip-integ'
 
-    use 'sheerun/vim-polyglot'
-    use 'metakirby5/codi.vim'
-    use 'terrortylor/nvim-comment'
-    use 'nvim-lua/plenary.nvim'
-    use 'lewis6991/gitsigns.nvim'
-    use 'nvim-treesitter/nvim-treesitter'
-    use 'norcalli/nvim-colorizer.lua'
-    use 'ryanoasis/vim-devicons'
-    use 'sbdchd/neoformat'
-    use 'windwp/nvim-autopairs'
-    use 'alvan/vim-closetag'
-    use 'tweekmonster/startuptime.vim'
-    use 'onsails/lspkind-nvim'
     use 'nvim-telescope/telescope.nvim'
     use 'nvim-telescope/telescope-media-files.nvim'
+    use 'metakirby5/codi.vim'
+    use 'lewis6991/gitsigns.nvim'
+    use 'windwp/nvim-autopairs'
+    use 'alvan/vim-closetag'
+    use 'onsails/lspkind-nvim'
     use 'nvim-lua/popup.nvim'
     end
 )
